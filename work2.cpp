9709f06897a9b218ccb67d7f6a261997898d1369
江沢民 <32710728+ZeminJiang1926@users.noreply.github.com>
2017-11-05 11:24:51 +0800

    Add files via upload

---
+++ work2.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int af();
+int bf();
+int cf();
+int df();
+int ef();
+int main()
+{
+	af();
+	bf();
+	cf();
+	df();
+	ef();
+    return 0;
+}
+int af()
+{
+	printf("小葵，你怎么哭了啊？\n");
+	return 0;
+}
+int bf()
+{
+	printf("恋爱这种事，眼光太低没有好结果！\n");
+	return 0;
+}
+int cf()
+{
+	printf("没事，那咱就眼光高一点！\n");
+	return 0;
+}
+int df()
+{
+	printf("可眼光太高，根本就没有结果！\n");
+	return 0;
+}
+int ef()
+{
+	printf("蛤？\n");
+	return 0;
+}
